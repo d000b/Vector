06152d3635bb9d102f5c183cb35a19ad2417359c
d000b <makc.Hitman@gmail.com>
2021-08-24 15:51:05 +0800

    Create MultiVector.h

---
+++ MultiVector.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace UltimaAPI
+{
+	template <typename __type__>
+	class MultiVector
+	{
+		using value = __type__;
+		using rvalue = value&&;
+		using pointer = value*;
+		using reference = value&;
+	protected:
+
+	public:
+
+	};
+}
